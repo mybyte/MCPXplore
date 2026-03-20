8f8578aeb3088bae83b01da088d39823cda8993c
= <=>
2026-03-20 16:04:32 +0100

    sandbox

--- src/main/index.ts
+++ src/main/index.ts
@@ -12,7 +12,7 @@ function createWindow(): void {
     trafficLightPosition: { x: 15, y: 10 },
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
+      sandbox: true
     }
   })
 
